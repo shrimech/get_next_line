148e912931c2ecebededb89ab077dd4040de4c9b
shrimech <https://shrimech:ghp 'sJhuOIz1hDmHfc3W73mjw9VufzgpRF036LL2@github.com>
2024-12-17 06:11:40 +0100

    i need to check leaaaks git add .git add .!

--- get_next_line.h
+++ get_next_line.h
@@ -1,24 +1,18 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
-# ifndef GET_NEXT_LINE
-# define GET_NEXT_LINE
-# ifndef buffer_size
-# define buffer_size 42
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-#endif
-
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <stdio.h>
-
-char    *get_next_line(int fd);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-void	*ft_memcpy(void *dst, const void *src);
-size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strdup(const char *s1);
-char	*ft_strchr(const char *str, int c);
+char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *str);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -13,184 +13,91 @@
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-
-char	*ft_strchr(const char *str, int c)
-{
-	int				i;
-	unsigned char	ch;
-
-	ch = (unsigned char)c;
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == ch)
-			return ((char *)&str[i]);
-		i++;
-	}
-	if (str[i] == ch)
-	{
-		return ((char *)&str[i]);
-	}	
-	return (NULL);
-}
-
-char	*ft_strdup(const char *s1)
-{
-	char	*s2;
-	size_t	len;
-
-	len = ft_strlen(s1) + 1;
-	s2 = malloc(len);
-	if (!s2)
-		return (NULL);
-	ft_strlcpy(s2, s1, len);
-	return (s2);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*s;
-	size_t	len;
-	int		i;
-	int		j;
-
-	if (!s1 || !s2)
-		return (NULL);
-	len = ft_strlen(s1) + ft_strlen(s2);
-	s = malloc(len + 1);
-	if (!s)
-		return (NULL);
-	j = 0;
-	while (s1[j])
-	{
-		s[j] = s1[j];
-		j++;
-	}
-	i = 0;
-	while (s2[i])
-	{
-		s[j + i] = s2[i];
-		i++;
-	}
-	s[i + j] = '\0';
-	return (s);
-}
-
-size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+size_t	ft_strlen(const char *str)
 {
 	size_t	i;
 
 	i = 0;
-	if (size != 0)
-	{
-		while (src[i] && i < (size - 1))
-		{
-			dest[i] = src[i];
-			i++;
-		}
-		dest[i] = '\0';
-	}
-	return (ft_strlen(src));
+	while (str[i])
+		i++;
+	return (i);
 }
 
-void	*ft_memcpy(void *dst, const void *src)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char		*dp;
-	const char	*sp;
+	int		i;
+	int		j;
+	char	*str;
 
-	dp = (char *)dst;
-	sp = (const char *)src;
-	if (!dp && !sp)
-		return (dst);
-	while (src)
+	if (!s1 || !s2)
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	i = -1;
+	j = 0;
+	while (s1[++i])
+		str[j++] = s1[i];
+	i = -1;
+	while (s2[++i])
+		str[j++] = s2[i];
+	str[j] = '\0';
+	return (str);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	c = (unsigned char)c;
+	i = -1;
+	while (s[++i])
 	{
-		*dp++ = *sp++;
+		if (s[i] == c)
+			return ((char *)&s[i]);
 	}
-	return (dst);
+	if (c == '\0')
+		return ((char *)&s[i]);
+	return (NULL);
 }
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*res;
-	char	*src;
-	size_t	reslen;
+	char	*str;
+	size_t	size;
+	size_t	i;
 
 	if (!s)
 		return (NULL);
-	if (ft_strlen(s) < (size_t)start)
-		return (ft_strdup(""));
-	src = (char *)s + start;
-	if (ft_strlen(src) < len)
-		reslen = ft_strlen(src) + 1;
-	else
-		reslen = len + 1;
-	res = malloc(reslen);
-	if (!res)
+	size = ft_strlen(s);
+	if (start > size)
+		len = 0;
+	if (size - start < len)
+		len = size - start;
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
 		return (NULL);
-	ft_strlcpy(res, src, reslen);
-	return (res);
+	i = -1;
+	while (++i < len)
+		str[i] = s[start + i];
+	str[i] = '\0';
+	return (str);
 }
 
-char    *get_next_line(int fd)
+char	*ft_strdup(const char *str)
 {
-    static char *save;
-    char        *buf;
-    char        *newline_pos;
-    char        *line;
-    char        *temp;
-    ssize_t     bytes_read;
+	char	*dup;
+	int		i;
 
-	if (fd < 0 || buffer_size <= 0)
-		return (NULL);
-    buf = malloc(buffer_size + 1);
-    if (!buf)
-        return (NULL);
-    while ((bytes_read = read(fd, buf, buffer_size)) > 0)
-    {
-        buf[bytes_read] = '\0';
-        if (save)
-            save = ft_strjoin(save, buf);
-        else
-            save = ft_strdup(buf);
-        if ((newline_pos = ft_strchr(save, '\n')))
-            break;
-    }
-    free(buf);
-    if (bytes_read < 0 || (!save && bytes_read == 0))
-        return (NULL);
-
-    newline_pos = ft_strchr(save, '\n');
-    if (newline_pos)
-    {
-        line = ft_substr(save, 0, newline_pos - save + 1);
-        temp = ft_strdup(newline_pos + 1);
-        free(save);
-        save = temp;
-    }
-    else
-    {
-        line = ft_strdup(save);
-        free(save);
-        save = NULL;
-    }
-    return (line);
-}
-int main()
-{
-	char *str;
-	int fd = open("salah.txt", O_RDONLY);
-
-	while ((str = get_next_line(fd)) != NULL)
-		printf("%s",str);
+	dup = (char *)malloc((ft_strlen(str) + 1) * sizeof(char));
+	if (!dup)
+		return (0);
+	i = 0;
+	while (str[i])
+	{
+		dup[i] = str[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
 }
